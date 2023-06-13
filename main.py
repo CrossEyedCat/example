c832c8c7661c95951d32bc80209fbb1cd55903fe
Artem <ArtemZenkevich1@yandex.ru>
2023-06-13 17:43:18 +0100

    ver 1.7

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import streamlit as st
 from youtube_transcript_api import YouTubeTranscriptApi
-import torch
 
-model, example_texts, languages, punct, apply_te = torch.hub.load(repo_or_dir='snakers4/silero-models',
-                                                                  model='silero_te')
 address = st.text_input("Input_youtube_address:")
 text = ""
 source = YouTubeTranscriptApi.get_transcript(address, languages=['ru'])
 for i in source:
     text=text+" "+i['text']
-st.write(apply_te(text, lan='ru'))
+st.write(text)
