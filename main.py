621d3cc65450f3f098e9f140a4c9f2300be07337
Artem <ArtemZenkevich1@yandex.ru>
2023-06-13 16:11:07 +0100

    ver 1.5

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from youtube_transcript_api import YouTubeTranscriptApi
 address = st.text_input("Input_youtube_address:")
 text = ""
 for i in YouTubeTranscriptApi.get_transcript(address, languages=['ru']):
-    text.__add__(i["text"])
+    text.__add__(i[text])
 st.write(text)
