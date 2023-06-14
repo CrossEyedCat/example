0e2fee0f5b036d17806b646722ea73403810ae77
Artem <ArtemZenkevich1@yandex.ru>
2023-06-14 10:58:11 +0100

    ver 1.7

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from youtube_transcript_api import YouTubeTranscriptApi
+import openai
 
 address = st.text_input("Input_youtube_address:")
 text = ""
 source = YouTubeTranscriptApi.get_transcript(address, languages=['ru'])
 for i in source:
-    text=text+" "+i['text']
+    text=text+". "+i['text']
 st.write(text)
